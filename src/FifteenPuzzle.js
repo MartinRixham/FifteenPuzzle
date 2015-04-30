ac9f5f896d664ab8b1258d93f74044d1db9bb220
Martin <martin.rixham@gmail.com>
2015-04-30 23:10:05 +0100

    Tile movement logic.

--- src/FifteenPuzzle.js
+++ src/FifteenPuzzle.js
@@ -15,31 +15,106 @@ function FifteenPuzzle() {
 		}
 	});
 
+	var self = this;
+
 	this.tiles = [
 
-		new Tile(1),
-		new Tile(2),
-		new Tile(3),
-		new Tile(4),
-		new Tile(5),
-		new Tile(6),
-		new Tile(7),
-		new Tile(8),
-		new Tile(9),
-		new Tile(10),
-		new Tile(11),
-		new Tile(12),
-		new Tile(13),
-		new Tile(14),
-		new Tile(15)
+		new Tile(1, function() { return self.tiles; }),
+		new Tile(2, function() { return self.tiles; }),
+		new Tile(3, function() { return self.tiles; }),
+		new Tile(4, function() { return self.tiles; }),
+		new Tile(5, function() { return self.tiles; }),
+		new Tile(6, function() { return self.tiles; }),
+		new Tile(7, function() { return self.tiles; }),
+		new Tile(8, function() { return self.tiles; }),
+		new Tile(9, function() { return self.tiles; }),
+		new Tile(10, function() { return self.tiles; }),
+		new Tile(11, function() { return self.tiles; }),
+		new Tile(12, function() { return self.tiles; }),
+		new Tile(13, function() { return self.tiles; }),
+		new Tile(14, function() { return self.tiles; }),
+		new Tile(15, function() { return self.tiles; }),
+		new Space()
 	];
 
-	function Tile(number) {
+	function Tile(number, tilesAccessor) {
 
 		this.number = new Binding({
 
-			text: function() { return number; }
+			text: function() { return number; },
+			click: function() {
+
+				if (this.isAdjacentToSpace()) {
+
+					var tiles = tilesAccessor();
+
+					var spaceIndex = this.getSpaceIndex();
+					var myIndex = this.getMyIndex();
+
+					tiles.splice(myIndex, 1, new Space());
+					tiles.splice(spaceIndex, 1, this);
+				}
+			}
 		});
+
+		this.getSpaceIndex = function() {
+
+			var tiles = tilesAccessor();
+
+			for (var i = 0; i < tiles.length; i++) {
+
+				if (tiles[i] instanceof Space) {
+
+					return i;
+				}
+			}
+		};
+
+		this.getMyIndex = function() {
+
+			var tiles = tilesAccessor();
+
+			for (var i = 0; i < tiles.length; i++) {
+
+				if (this == tiles[i]) {
+
+					return i;
+				}
+			}
+		};
+
+		this.isAdjacentToSpace = function() {
+
+			var tiles = tilesAccessor();
+
+			var myIndex = this.getMyIndex();
+
+			if (tiles[myIndex - 1] instanceof Space) {
+
+				return true;
+			}
+
+			if (tiles[myIndex + 1] instanceof Space) {
+
+				return true;
+			}
+
+			if (tiles[myIndex - 4] instanceof Space) {
+
+				return true;
+			}
+
+			if (tiles[myIndex + 4] instanceof Space) {
+
+				return true;
+			}
+
+			return false;
+		};
+	}
+
+	function Space() {
+
 	}
 }
 
